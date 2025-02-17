2cc0c53cdccf1a24b14340538dcb7e823ebf6320
Mark Hall <markhall@obsidian-logic.com>
2025-02-17 13:01:49 -0800

    Adds Xcode project

---
+++ R•Fetch/R•Fetch/R_FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_FetchApp.swift
+//  R•Fetch
+//
+//  Created by Mark Hall on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct R_FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ R•Fetch/R•FetchTests/R_FetchTests.swift
@@ -0,0 +1,16 @@
+//
+//  R_FetchTests.swift
+//  R•FetchTests
+//
+//  Created by Mark Hall on 2/17/25.
+//
+
+import Testing
+
+struct R_FetchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
